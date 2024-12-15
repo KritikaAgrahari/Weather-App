667ab39d99fec388521069d16f3fcfde65172c2d
Kritika-2 <kritikaagrahari42@gmail.com>
2024-12-15 22:18:02 +0530

    weather.css is created to include all the css styling applied to weather.jsx file

---
+++ src/components/Weather.jsx
@@ -0,0 +1,11 @@
+
+const Weather = () => {
+  return (
+    <div className="weather">
+        <center><input type="text" placeholder="search city"></input></center>
+      
+    </div>
+  )
+}
+
+export default Weather;
